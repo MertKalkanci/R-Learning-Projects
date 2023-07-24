ba82bf099314f46ccca62a931bd416ff84b53122
Mert Kalkancı <63818504+MertKalkanci@users.noreply.github.com>
2023-07-24 14:40:13 +0300

    Update normal_distrubation.r

--- General/normal_distrubation.r
+++ General/normal_distrubation.r
@@ -9,7 +9,7 @@ print(head(df))
 
 
 plot <- ggplot(data = df, aes(x = Xval, y = Yval)) + 
-  geom_line(aes(color = Xval))+
-  labs(title = "Normal Distribution", x = "X", y = "Frequency", color = "X Value")
+  geom_line(aes(color = Yval))+
+  labs(title = "Normal Distribution", x = "X", y = "Frequency", color = "Y Value")
 
 show(plot)
